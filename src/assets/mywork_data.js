b541f10dae1d06b60c5dbcfbf397074bd7c0fd12
Rugashan Jeevarajah <87310696+IT21054822@users.noreply.github.com>
2025-06-19 10:37:34 +0530

    bug fixes

--- src/assets/mywork_data.js
+++ src/assets/mywork_data.js
@@ -3,7 +3,7 @@ import project2_img from '../assets/project_2.png'
 import project3_img from '../assets/project_3.png'
 import project4_img from '../assets/project_4.png'
 import project5_img from '../assets/project_5.png'
-import project6_img from '../assets/screens.png'
+import project6_img from '../assets/Screens.png'
 
 const mywork_data = [
     {
