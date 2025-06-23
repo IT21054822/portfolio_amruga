ddc571aff6b6711059fef9010c1a32449f14b272
Rugashan Jeevarajah <87310696+IT21054822@users.noreply.github.com>
2025-06-23 16:25:01 +0530

    Experience

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -27,7 +27,7 @@ const Navbar = () => {
             <img onClick={closeMenu} className='nav-mob-close' src={menu_close} alt="" />
               <li><AnchorLink className='anchor-link' href='#home'><p onClick={()=>setMenu("home")}>Home</p></AnchorLink>{menu==="home" ? <img src={underline} alt=' '/> : <></> }</li>
               <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>About Me</p></AnchorLink> {menu==="about" ? <img src={underline} alt=' '/> : <></> }</li>
-              <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")}>Services</p></AnchorLink>{menu==="services" ? <img src={underline} alt=' '/> : <></> }</li>
+              <li><AnchorLink className='anchor-link' offset={50} href='#services'><p onClick={()=>setMenu("services")}>Experience</p></AnchorLink>{menu==="services" ? <img src={underline} alt=' '/> : <></> }</li>
               <li><AnchorLink className='anchor-link' offset={50} href='#mywork'><p onClick={()=>setMenu("portfolio")}>Portfolio</p></AnchorLink>{menu==="portfolio" ? <img src={underline} alt=' '/> : <></> }</li>
               <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink>{menu==="contact" ? <img src={underline} alt=' '/> : <></> }</li>
           </ul>
